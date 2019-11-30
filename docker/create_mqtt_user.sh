23bea72501c0574c63bd7e9aa0951d2956b0a6ea
Marco Bakera <marco@bakera.de>
2019-11-30 09:58:53 +0100

    adding users to aclfile

--- docker/create_mqtt_user.sh
+++ docker/create_mqtt_user.sh
@@ -11,5 +11,14 @@ user_pass=$1
 
 echo Creating mqtt user $user_pass
 
+# create user and update aclfile
 mosquitto_passwd -b passwdfile $user_pass $user_pass
+echo "# new user added $(date)" >> aclfile
+echo "user ${user_pass}" >> aclfile
+# https://manpages.debian.org/buster/mosquitto/mosquitto.conf.5.en.html
+echo "topic read mmsnake/world" >> aclfile
+echo "topic readwrite mmsnake/snake/${user_pass}/move" >> aclfile
+
+# TODO seems not to reread the passwdfile
+# let mosquitto (pid 1) reread the config
 kill -SIGHUP 1
