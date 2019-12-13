7da87b24b41a5fae95443b63a95da94a7c8b0bdd
Marco Bakera <marco@bakera.de>
2019-12-13 14:55:41 +0100

    using gunicorn to start the flask app.

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -4,4 +4,5 @@
 
 mosquitto -d -c /etc/mosquitto/mosquitto.conf
 # add for debugging: FLASK_ENV=development
-cd /web; FLASK_APP=web.py  flask run --host=0.0.0.0 --port 9090
+#cd /web; FLASK_APP=web.py  flask run --host=0.0.0.0 --port 9090
+cd /web; gunicorn -w 4 -b 0.0.0.0:9090 web:app
